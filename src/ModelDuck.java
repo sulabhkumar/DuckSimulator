c4624cb2fbcfedd0086694721794b8e4f813d6bd
Sulabh Kumar <sulabhkumar0@gmail.com>
2019-10-09 11:23:06 +0530

    Duck(Strategy Pattern)

---
+++ src/ModelDuck.java
@@ -0,0 +1,11 @@
+public class ModelDuck extends Duck{
+    public ModelDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior= new Quack();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I am a model Duck!!");
+    }
+}
